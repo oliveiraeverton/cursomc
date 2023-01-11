1166208fe8657d7c12e65c1e0ae038ee3f23a2e3
ev_oli <1112everton@gmail.com>
2023-01-11 09:38:13 -0400

    Testando a primeira Classe de Domínio - Categoria

--- src/main/java/com/evertonoliveira/resources/CategoriaResource.java
+++ src/main/java/com/evertonoliveira/resources/CategoriaResource.java
@@ -1,15 +1,30 @@
 package com.evertonoliveira.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.evertonoliveira.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
